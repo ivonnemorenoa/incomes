b4ad25de5e4bab73a59488b4e41b5b7d7c087c1e
Ivonne <ivonne.morenoa@hotmail.com>
2023-07-03 19:20:06 -0600

    news components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { IncomesComponent } from './incomes/incomes.component';
+import { StatusComponent } from './status/status.component';
+import { HomeComponent } from './home/home.component';
+import { PolicyComponent } from './policy/policy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IncomesComponent,
+    StatusComponent,
+    HomeComponent,
+    PolicyComponent
   ],
   imports: [
     BrowserModule,
