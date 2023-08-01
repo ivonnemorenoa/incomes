eb79c29cfaca15047066c0de8760d4f6d368011f
Ivonne <ivonne.morenoa@hotmail.com>
2023-08-01 17:40:27 -0600

    eliminacion de componentes

--- src/app/incomes/incomes.component.ts
+++ src/app/incomes/incomes.component.ts
@@ -1,22 +1,15 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormArray, FormBuilder, Validators, AbstractControl } from '@angular/forms';
-import { SystemCatalog } from 'src/app/shared/lib/system-catalog';
-import { buildFormArrayOptionsForm } from '../components/form-control-checkbox/form-control-checkbox.component';
-// import { ControlSelect, FormSelect, buildFormSelect } from '../components/form-control-select/form-control-select.component';
 
-// export interface IncomeForm {
-//   bankname: FormArray<FormControl<string | null>>;
-//   amount: FormControl<string | null>;
-//   currency: FormControl<string | null>;
-// }
+import { SystemCatalog } from '../shared/lib/system-catalog';
 
-// export interface IncomeFormValue {
-//   bankname: ControlSelect[],
-//   amount: string,
-//   currency: string,
-// }
 
-// export type ServiceType = 'CITIBANAMEX' | 'BBVA' | 'SCOTIABANK' | 'HSBC';
+export interface incomesForm{
+
+}
+
+export interface incomesFormValue{
+  
+}
 @Component({
   selector: 'app-incomes',
   templateUrl: './incomes.component.html',
@@ -25,42 +18,7 @@ import { buildFormArrayOptionsForm } from '../components/form-control-checkbox/f
 
 export class IncomesComponent {
 
-  // public get bankname() { return this.form.get('bankname') as FormArray; }
-  // public get amount() { return this.form.get('amount') as FormControl; }
-  // public get currency() { return this.form.get('currency') as FormControl; }
-
-
-
-  // public form: any = FormGroup<IncomeForm>;
-
-
-  // constructor(
-  //   private formBuilder: FormBuilder
-  // ) {
-  //   this.form = this.formBuilder.group<IncomeForm>({
-			
-  //     bankname: buildFormArrayOptionsForm<string>([{
-	// 			key: 'bank1',
-	// 			description: 'CITIBANAMEX'
-	// 		},
-	// 		{
-	// 			key: 'bank2',
-	// 			description: 'BBVA'
-	// 		},
-  //     {
-	// 			key: 'bank3',
-	// 			description: 'SCOTIABANK'
-	// 		}
-	// 		], ['bank1']),
-  //     amount: new FormControl('', [Validators.required]),
-  //     currency: new FormControl('', [Validators.required]),
-	// 	});
-
-  // };
-
-  // public onSubmit() {
-  //   console.log(this.form.value);
-  // }
+ 
 }
 
 
